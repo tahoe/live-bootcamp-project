0df31394753e1f2136545012df4c3018372570a5
Dennis Durling <djdtahoe@gmail.com>
2025-07-25 19:47:10 -0700

    changed listen addresses to localhost too

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -2,7 +2,7 @@ use auth_service::Application;
 
 #[tokio::main]
 async fn main() {
-    let app = Application::build("0.0.0.0:3000")
+    let app = Application::build("127.0.0.1:3000")
         .await
         .expect("Failed to build app");
 
