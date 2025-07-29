661983e5bbccb5008153228d2ba2d9393d10b047
Dennis Durling <djdtahoe@gmail.com>
2025-07-29 13:02:19 -0700

    [Sprint 2][Task 1] Step 1: 201 test case is failing

--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -8,15 +8,37 @@ async fn should_return_422_if_malformed_input() {
 
     // TODO: add more malformed input test cases
     let test_cases = [serde_json::json!({
+        "email":&random_email,
+        "requires2FA": true
+    })];
+
+    for test_case in test_cases.iter() {
+        let response = app.post_signup(test_case).await;
+        assert_eq!(
+            response.status().as_u16(),
+            422,
+            "Failed for input: {test_case}"
+        );
+    }
+}
+
+#[tokio::test]
+async fn should_return_201_if_valid_input() {
+    let app = TestApp::new().await;
+
+    let random_email = get_random_email();
+
+    let test_cases = [serde_json::json!({
+        "email": random_email,
         "password": "pass123",
         "requires2FA": true
     })];
 
     for test_case in test_cases.iter() {
-        let response = app.post_signup(&random_email).await;
+        let response = app.post_signup(test_case).await;
         assert_eq!(
             response.status().as_u16(),
-            422,
+            201,
             "Failed for input: {test_case}"
         );
     }
