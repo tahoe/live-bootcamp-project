f179dc06ae0835b5cf2e7cea208125e66f77a2df
Dennis Durling <djdtahoe@gmail.com>
2025-07-29 22:24:27 -0700

    [Sprint 2][Task 1] Step 4 extra, added constructor to HashmapUserStore

--- auth-service/src/services/hashmap_user_store.rs
+++ auth-service/src/services/hashmap_user_store.rs
@@ -16,6 +16,11 @@ pub struct HashmapUserStore {
 }
 
 impl HashmapUserStore {
+    pub fn new() -> HashmapUserStore {
+        let users = HashMap::new();
+        HashmapUserStore { users }
+    }
+
     pub fn add_user(&mut self, user: User) -> Result<(), UserStoreError> {
         if self.users.contains_key(&user.email.to_owned()) {
             return Err(UserStoreError::UserAlreadyExists);
@@ -54,16 +59,14 @@ mod tests {
     #[tokio::test]
     async fn test_add_user() {
         let user = User::new("test@test.com".to_owned(), "password".to_owned(), true);
-        let users = HashMap::new();
-        let mut mapper = HashmapUserStore { users };
+        let mut mapper = HashmapUserStore::new();
         let _ = mapper.add_user(user);
     }
 
     #[tokio::test]
     async fn test_get_user() {
         let user = User::new("test@test.com".to_owned(), "password".to_owned(), true);
-        let users = HashMap::new();
-        let mut mapper = HashmapUserStore { users };
+        let mut mapper = HashmapUserStore::new();
         let _ = mapper.add_user(user.clone());
         assert_eq!(mapper.get_user("test@test.com"), Ok(user.clone()));
     }
@@ -71,8 +74,7 @@ mod tests {
     #[tokio::test]
     async fn test_validate_user() {
         let user = User::new("test@test.com".to_owned(), "password".to_owned(), true);
-        let users = HashMap::new();
-        let mut mapper = HashmapUserStore { users };
+        let mut mapper = HashmapUserStore::new();
         let _ = mapper.add_user(user.clone());
         assert_eq!(mapper.validate_user("test@test.com", "password"), Ok(()));
     }
